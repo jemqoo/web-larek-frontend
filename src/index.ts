8022380aecd7a707f83ca20b0c180d79d0e03126
jemqoo <y.soldatenkowaa@gmail.com>
2024-02-28 01:12:03 +0800

    1.0

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,40 @@
 import './scss/styles.scss';
 
+// интерфейс, описывающий карточку товара
+export interface ILotItem {
+	id: string;
+	title: string;
+	category: string;
+	description?: string;
+	image: string;
+	price: string | number;
+}
+
+// интерфейс модели приложения
+export interface IAppState {
+	catalog: ILotItem[];
+	basket: string[];
+	order: IOrder | null;
+}
+
+// Интерфейс для формы заказа
+export interface IOrderForm {
+	payment: string;
+	email: string;
+	phone: string;
+	address: string;
+	total: number;
+	items: string[];
+}
+
+// Интерфейс для контактной формы
+export interface IContactForm {
+	email: string;
+	phone: string;
+}
+
+//Интерфейс для формы
+export interface IOrder extends IOrderForm {
+	total: number;
+	items: string[];
+}
